1348176fe1198b7f4bb988f45e59d4741c504d0e
Thangavel <thangavel.ravichandran@ameexusa.com>
2020-04-16 14:20:48 +0530

    ec2metadata

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const bucket =  'private-uploadtestregion-1';
 const secretAccessKey =  process.env.S3_ACCESSKEY;
 const accessKeyId =  process.env.S3_ACCESSKEYID;
 console.log(process.env.PORT);
-AWS.config.credentials = {
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-  };
+// AWS.config.credentials = {
+//     accessKeyId: accessKeyId,
+//     secretAccessKey: secretAccessKey
+//   };
+AWS.config.credentials = new AWS.EC2MetadataCredentials({
+    httpOptions: { timeout: 5000 }, // 5 second timeout
+    maxRetries: 10, // retry 10 times
+    retryDelayOptions: { base: 200 } // see AWS.Config for information
+  });
   AWS.config.region = 'us-east-1';
    AWS.config.getCredentials(function(err) {
         if (err) console.log('aws credentials error', err.stack); // credentials not loaded
