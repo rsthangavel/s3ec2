e50bfa7b3b6809b15ce675bab094bc1fe5b2fe70
Thangavel <thangavel.ravichandran@ameexusa.com>
2020-04-19 20:15:55 +0530

    Remove credentials

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const accessKeyId = process.env.S3_ACCESSKEYID;
 //     accessKeyId: accessKeyId,
 //     secretAccessKey: secretAccessKey
 //   };
-AWS.config.credentials = {};
-AWS.config.credentials = new AWS.EC2MetadataCredentials({
-    httpOptions: { timeout: 5000 }, // 5 second timeout
-    maxRetries: 10, // retry 10 times
-    retryDelayOptions: { base: 200 } // see AWS.Config for information
-});
-AWS.config.region = 'us-east-1';
-AWS.config.getCredentials(function (err) {
-    if (err) console.log('aws credentials error', err.stack); // credentials not loaded
+// AWS.config.credentials = {};
+// AWS.config.credentials = new AWS.EC2MetadataCredentials({
+//     httpOptions: { timeout: 5000 }, // 5 second timeout
+//     maxRetries: 10, // retry 10 times
+//     retryDelayOptions: { base: 200 } // see AWS.Config for information
+// });
+// AWS.config.region = 'us-east-1';
+// AWS.config.getCredentials(function (err) {
+//     if (err) console.log('aws credentials error', err.stack); // credentials not loaded
     
-    else {
-        console.log(AWS.config.credentials);
-        console.log('AWS Credentials Loaded');
-    }
-});
+//     else {
+//         console.log(AWS.config.credentials);
+//         console.log('AWS Credentials Loaded');
+//     }
+// });
 
 
 const s3 = new AWS.S3();
